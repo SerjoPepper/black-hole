c19da301eb5dfd51f417c1c3db9589f246610065
SerjoPepper <SerjoPepper@gmail.com>
2011-08-10 01:36:22 +0400

    fix

--- blackhole.js
+++ blackhole.js
@@ -11,7 +11,6 @@
 	addClass(body, c);
 	
 	body.appendChild(blackhole);
-	removeClass(blackhole, 'init-' + cName('position'));
 	addClass(blackhole, 'active-' + cName('position'));
 	
 	function cName (className) {

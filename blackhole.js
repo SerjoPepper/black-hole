228694a588065ea51997a93b8599c263b2e25fa7
SerjoPepper <SerjoPepper@gmail.com>
2011-08-11 00:01:07 +0400

    add stuff

--- blackhole.js
+++ blackhole.js
@@ -1,22 +1,31 @@
 (function () {
-	var css = function (el, css) {
-		el.setAttribute('style', css)
-	},
-		c = cName('animate')
+	var c = cName('animate')
 		body = document.getElementsByTagName('body')[0],
 		blackhole = document.createElement('div'),
-		transparentLayout = document.createElement('div');
+		transparentLayout = document.createElement('div'),
+		windowOffset = window.pageYOffset,
+		screenHeight = window.screen.height;
 		
-	
+	css(blackhole, { left: window.screen.width/2; top: (windowOffset + screenHeight)/2 });
 	addClass(document.documentElement, cName('wrapper'));
 	addClass(transparentLayout, cName('transparent-layout'));
 	addClass(blackhole, 'init-' + cName('position'));
+	setTimeout(function(){ addClass(blackhole, 'active-' + cName('position')); }, 200);
 	addClass(body, c);
 	
 	body.appendChild(blackhole);
 	body.appendChild(transparentLayout);
 	
-	setTimeout(function(){ addClass(blackhole, 'active-' + cName('position')); }, 200)
+	
+	function css (el, css) {
+		if (typeof css == 'string') {
+			el.setAttribute('style', el.getAttribute('style') + css);
+		} else {
+			for (var k in css) {
+				el.style[k] = css[k];
+			}
+		}
+	},
 	
 	function cName (className) {
 		return 'blackhole-' + className;
@@ -31,5 +40,33 @@
 	function removeClass(o, c){
   		var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g")
     	o.className = o.className.replace(re, "$1").replace(/\s+/g, " ").replace(/(^ | $)/g, "")
+	}
+	
+	function offsetPosition (node, root) {
+		var node = this, left = 0, top = 0;
+		
+		if (node == root)
+			return { left: left, top: top };
+		
+		var parent, lastNode;
+		for (;;) {
+			left += node.offsetLeft;
+			top += node.offsetTop;
+			
+			parent = node.offsetParent;
+			if (parent && parent !== root) {
+				lastNode = node;
+				node = parent;
+			}
+			else {
+				if (lastNode) {
+					left -= lastNode.scrollLeft;
+					top -= lastNode.scrollTop;
+				}	
+				break;
+			}
+		}
+		
+		return { left: left, top: top };
 	}
 })();
